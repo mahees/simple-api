640d74a145358661a582fec868763d03e40a3892
mahees ishaq <mahees.ishaq@gmail.com>
2014-11-19 23:53:08 -0400

    wip

---
+++ routes/forms.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var router = express.Router();
+var Location = require('../models/location');
+
+router.get('/', function(req, res) {
+    Location.find(function(err, locations) {
+        if (err) {
+            return res.send(err);
+        }
+        res.json(locations);
+    });
+});
+
+router.post('/', function(req, res) {
+    var location = new Location(req.body);
+    location.markModified('meta');
+    location.save(function(err, loc) {
+        if (err) {
+            return res.send(err);
+        }
+        res.json(loc);
+    });
+});
+
+router.get('/:id/locations', function(req, res) {
+    Location.find({
+        formId : req.params.id
+    }, function(err, locations) {
+        if (err) {
+            return res.send(err);
+        }
+        res.json(locations);
+    });
+});
+
+router.get('/:id', function(req, res) {
+    Location.findById(req.params.id, function(err, location) {
+        if (err) {
+            return res.send(err);
+        }
+        res.json(location);
+    });
+});
+
+router.post('/:id/locations', function(req, res) {
+    var location = new Location(req.body);
+    location.formId = req.params.id;
+    location.markModified('meta');
+    location.save(function(err, loc) {
+        if (err) {
+            return res.send(err);
+        }
+        res.json(loc);
+    });
+});
+
+router.put('/:id', function(req, res) {
+    Location.findByIdAndUpdate(req.params.id, req.body, function(err, location) {
+        if (err) {
+            return res.send(err);
+        }
+        res.json(location);
+    });
+});
+
+router.delete('/:id', function(req, res) {
+    Location.remove({
+        _id : req.params.id
+    }, function(err, bear) {
+        if (err) {
+            return res.send(err);
+        }
+        res.send(200);
+    });
+});
+
+module.exports = router;
